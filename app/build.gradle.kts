0e0fd2be5712a44d42c0bc9e51106ae438360488
Felitendo <Felendo@web.de>
2024-07-19 23:57:54 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "io.github.felitendo.manager"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1002
-        versionName = "1.0.2"
+        versionCode = 101
+        versionName = "v1.0.1"
 
         buildConfigField("String", "GIT_BRANCH", "\"${getCurrentBranch()}\"")
         buildConfigField("String", "GIT_COMMIT", "\"${getLatestCommit()}\"")
